996d2e75d51a67a5852c5cc2adc76456816db27c
Bianca Petracca <biancapetracca@gmail.com>
2016-04-06 01:29:46 -0300

    Configuring server - proof of concept

---
+++ src/main/java/model/User.java
@@ -0,0 +1,21 @@
+package model;
+public class User {
+
+    private String nickName;
+    private String userName;
+
+    public User(String nickName, String userName){
+        this.nickName = nickName;
+        this.userName = userName;
+    }
+
+    public String getUserName(){
+        return userName;
+    }
+
+    public String getNickName(){
+        return nickName;
+    }
+
+
+}
