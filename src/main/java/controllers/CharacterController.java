0ee4a7580abc01329294898a1ab4e7758f27ae01
Sebastian Bruno <sebastianbruno.exe@gmail.com>
2016-04-11 21:36:25 -0300

    Primera versión de las urls

---
+++ src/main/java/controllers/CharacterController.java
@@ -0,0 +1,36 @@
+package controllers;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CharacterController {
+    /* API */
+    @RequestMapping(value="api/characters",method= RequestMethod.GET)
+    public String characters() {
+        return "List of characters";
+    }
+
+    @RequestMapping(value="api/character/{id}",method= RequestMethod.POST)
+    public String setAsFavorite(@PathVariable int id) {
+        return "Character setted as favorite";
+    }
+
+    @RequestMapping(value="/api/ranking",method= RequestMethod.GET)
+    public String ranking() {
+        return "Ranking of users";
+    }
+
+    /* FRONT END */
+    @RequestMapping(value="characters",method= RequestMethod.GET)
+    public String charactersPage() {
+        return "Characters Page";
+    }
+
+    @RequestMapping(value="/ranking",method= RequestMethod.GET)
+    public String rankingPage() {
+        return "Ranking Page";
+    }
+}
---
+++ src/main/java/controllers/SessionController.java
@@ -0,0 +1,29 @@
+package controllers;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SessionController {
+    /* API */
+    @RequestMapping(value="api/login",method= RequestMethod.POST)
+    public String login() {
+        return "User logged in";
+    }
+    @RequestMapping(value="api/logout",method= RequestMethod.POST)
+    public String logout() {
+        return "User signed out";
+    }
+
+    /* FRONT END */
+    @RequestMapping(value="/login",method= RequestMethod.GET)
+    public String loginPage() {
+        return "Login Page";
+    }
+    @RequestMapping(value="/logout",method= RequestMethod.GET)
+    public String logoutPage() {
+        return "Logout Page";
+    }
+}
