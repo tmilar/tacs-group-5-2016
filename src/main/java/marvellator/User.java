cea32b8c3285708ee499c51f44a2882c1ee5580a
Bianca Petracca <biancapetracca@gmail.com>
2016-04-12 02:03:29 -0300

    Static responses for users

--- src/main/java/marvellator/User.java
+++ src/main/java/marvellator/User.java
@@ -5,19 +5,25 @@ public class User {
     private String nickName;
     private String userName;
 
-    public User(String nickName, String userName){
+    public User(String nickName, String userName) {
         this.nickName = nickName;
         this.userName = userName;
     }
 
-    public String getUserName(){
+    public String getUserName() {
 
         return userName;
     }
 
-    public String getNickName(){
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getNickName() {
         return nickName;
     }
 
-
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
 }
