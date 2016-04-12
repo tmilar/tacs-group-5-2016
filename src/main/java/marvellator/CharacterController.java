35a75aa9b08d8e2845368742ce5bbc2c5613f971
Bianca Petracca <biancapetracca@gmail.com>
2016-04-12 10:15:33 -0300

    Fixed: urls
    Former-commit-id: ea7b836ffb56104fffd871b1ff08c06ba5e9214f

--- src/main/java/marvellator/CharacterController.java
+++ src/main/java/marvellator/CharacterController.java
@@ -8,28 +8,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CharacterController {
     /* API */
-    @RequestMapping(value="api/characters",method= RequestMethod.GET)
+    @RequestMapping(value = "api/characters", method = RequestMethod.GET)
     public String characters() {
         return "List of characters";
     }
 
-    @RequestMapping(value="api/character/{id}",method= RequestMethod.POST)
+    @RequestMapping(value = "api/character/{id}", method = RequestMethod.POST)
     public String setAsFavorite(@PathVariable int id) {
-        return "Character setted as favorite";
+        return "Character set as favorite. Its id is: " + id;
     }
 
-    @RequestMapping(value="/api/ranking",method= RequestMethod.GET)
+    @RequestMapping(value = "/api/ranking", method = RequestMethod.GET)
     public String ranking() {
         return "Ranking of users";
     }
 
     /* FRONT END */
-    @RequestMapping(value="characters",method= RequestMethod.GET)
+    @RequestMapping(value = "characters", method = RequestMethod.GET)
     public String charactersPage() {
         return "Characters Page";
     }
 
-    @RequestMapping(value="/ranking",method= RequestMethod.GET)
+    @RequestMapping(value = "/ranking", method = RequestMethod.GET)
     public String rankingPage() {
         return "Ranking Page";
     }
