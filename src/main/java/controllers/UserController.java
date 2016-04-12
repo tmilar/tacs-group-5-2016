cc70246ef9567cee14f68bb55845817154003a7d
Sebastian Bruno <sebastianbruno.exe@gmail.com>
2016-04-11 21:36:25 -0300

    Primera versión de las urls
    Former-commit-id: 8f98b1dded17158c9692367fd56f4ac9afcb1fcc

--- src/main/java/controllers/UserController.java
+++ src/main/java/controllers/UserController.java
@@ -1,14 +1,47 @@
 package controllers;
 
 import model.User;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
+    /* API */
     @RequestMapping("/user")
     public User userHome(@RequestParam(value="name", defaultValue="superuser") String name) {
         return new User("A default nickname",  name);
+    }
+    @RequestMapping(value="api/user",method= RequestMethod.POST)
+    public String userSignUp() {
+        return "User registered!";
+    }
+    @RequestMapping(value="api/users",method= RequestMethod.GET)
+    public String users() {
+        return "List of users";
+    }
+    @RequestMapping(value="api/users/{id}/favs",method= RequestMethod.GET)
+    public String usersFavorites(@PathVariable int id) {
+        return "List of users' favorites";
+    }
+    @RequestMapping(value="api/users/{id}/favs",method= RequestMethod.POST)
+    public String addFavorite(@PathVariable int id) {
+        return "Added favorite character";
+    }
+    @RequestMapping(value="api/users/{idUser}/favs/{idCharacter}",method= RequestMethod.DELETE)
+    public String deleteFavorite(@PathVariable int id) {
+        return "Deleted favorite character";
+    }
+
+    /* FRONT END */
+    @RequestMapping(value="/register",method= RequestMethod.GET)
+    public String registerPage() {
+        return "Register Page";
+    }
+    @RequestMapping(value="users",method= RequestMethod.GET)
+    public String usersPage() {
+        return "Users Page";
+    }
+    @RequestMapping(value="users/{id}/favs",method= RequestMethod.GET)
+    public String favoritesPage(@PathVariable int id) {
+        return "Favorites Page";
     }
 }
