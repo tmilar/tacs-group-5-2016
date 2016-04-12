85791d8ea3eb046aa08e2e5ff2f01250d0d3c6f4
Bianca Petracca <biancapetracca@gmail.com>
2016-04-12 10:15:33 -0300

    Fixed: urls

--- src/main/java/marvellator/UserController.java
+++ src/main/java/marvellator/UserController.java
@@ -1,55 +1,30 @@
-package marvellator;/*package controllers;
+package marvellator;
 
 import marvellator.User;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-public class marvellator.UserController {
-    *//* API *//*
+public class UserController {
+
+    //* API *//*
 
     @RequestMapping(value="api/users/{id}/favs",method= RequestMethod.GET)
     public String usersFavorites(@PathVariable int id) {
-        return "List of users' favorites";
+        return "List of user" + id + "'s favorites";
     }
     @RequestMapping(value="api/users/{id}/favs",method= RequestMethod.POST)
     public String addFavorite(@PathVariable int id) {
-        return "Added favorite character";
+        return "Added favorite character, its id is: " + id;
     }
     @RequestMapping(value="api/users/{idUser}/favs/{idCharacter}",method= RequestMethod.DELETE)
-    public String deleteFavorite(@PathVariable int id) {
-        return "Deleted favorite character";
+    public String deleteFavorite(@PathVariable int idUser, @PathVariable int idCharacter) {
+        return "Deleted favorite character. The user was: " + idUser + "The character: " + idCharacter;
     }
 
-    *//* FRONT END *//*
-    @RequestMapping(value="/register",method= RequestMethod.GET)
-    public String registerPage() {
-        return "Register Page";
-    }
-    @RequestMapping(value="users",method= RequestMethod.GET)
-    public String usersPage() {
-        return "Users Page";
-    }
-    @RequestMapping(value="users/{id}/favs",method= RequestMethod.GET)
-    public String favoritesPage(@PathVariable int id) {
-        return "Favorites Page";
-    }
-}*/
-
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-
-@Controller
-public class UserController {
-
-    /* @RequestMapping(value="/user", method=RequestMethod.GET)
-     public String greetingForm(Model model) {
-         model.addAttribute("user", new User("name", "username"));
-         return "user";
-     }*/
+    /*FRONT END*/
     @RequestMapping(value = "/user", method = RequestMethod.GET)
     public User userHome(@RequestParam(value = "name", defaultValue = "A default name :)") String name) {
         return new User("A default nickname. We will only be receiving a userName. It is: ", name);
@@ -62,6 +37,16 @@ public class UserController {
     @RequestMapping(value="/users",method= RequestMethod.GET)
     public User users(@RequestParam(value = "id", defaultValue = "A cute static id ") String name) {
         return new User("This is a default id, we are making a static response :) ", name);
+    }
+
+    @RequestMapping(value="/register",method= RequestMethod.GET)
+    public String registerPage() {
+        return "Register Page";
+    }
+
+    @RequestMapping(value="users/{id}/favs",method= RequestMethod.GET)
+    public String favoritesPage(@PathVariable int id) {
+        return "Favorites Page,the id is: " + id;
     }
 
 }
