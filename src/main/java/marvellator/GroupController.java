4525c075011e536845fb89178fd8df3783e60351
Bianca Petracca <biancapetracca@gmail.com>
2016-04-12 10:15:33 -0300

    Fixed: urls
    Former-commit-id: 52448b3dc73ca635f6d041f6e974ec5e64e46c07

--- src/main/java/marvellator/GroupController.java
+++ src/main/java/marvellator/GroupController.java
@@ -8,33 +8,38 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GroupController {
     /* API */
-    @RequestMapping(value="api/groups",method= RequestMethod.GET)
+    @RequestMapping(value = "api/groups", method = RequestMethod.GET)
     public String groups() {
         return "List of groups";
     }
-    @RequestMapping(value="api/groups",method= RequestMethod.POST)
+
+    @RequestMapping(value = "api/groups", method = RequestMethod.POST)
     public String newGroup() {
         return "New group";
     }
-    @RequestMapping(value="api/groups/{id}",method= RequestMethod.GET)
+
+    @RequestMapping(value = "api/groups/{id}", method = RequestMethod.GET)
     public String group(@PathVariable int id) {
         return "A group";
     }
-    @RequestMapping(value="api/groups/{id}",method= RequestMethod.PUT)
+
+    @RequestMapping(value = "api/groups/{id}", method = RequestMethod.PUT)
     public String addCharacter(@PathVariable int id) {
         return "Added a character to group";
     }
-    @RequestMapping(value="api/groups/{id}",method= RequestMethod.DELETE)
+
+    @RequestMapping(value = "api/groups/{id}", method = RequestMethod.DELETE)
     public String deleteGroup(@PathVariable int id) {
         return "Group deleted";
     }
 
     /* FRONT END */
-    @RequestMapping(value="/groups",method= RequestMethod.GET)
+    @RequestMapping(value = "/groups", method = RequestMethod.GET)
     public String groupsPage() {
         return "Groups Page";
     }
-    @RequestMapping(value="/groups/{id}",method= RequestMethod.GET)
+
+    @RequestMapping(value = "/groups/{id}", method = RequestMethod.GET)
     public String groupPage(@PathVariable int id) {
         return "A group Page";
     }
--- src/main/java/marvellator/SessionController.java
+++ src/main/java/marvellator/SessionController.java
@@ -5,23 +5,26 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class SessionController {
     /* API */
-    @RequestMapping(value="/login",method= RequestMethod.POST)
+    @RequestMapping(value = "api/login", method = RequestMethod.POST)
     public String login() {
-        String id = "asdsfdsf";
-        return id;
+        return "Login page";
     }
-    @RequestMapping(value="api/logout",method= RequestMethod.POST)
+
+    @RequestMapping(value = "api/logout", method = RequestMethod.POST)
     public String logout() {
-        return "marvellator.User signed out";
+
+        return "User signed out";
     }
 
     /* FRONT END */
-    @RequestMapping(value="/login",method= RequestMethod.GET)
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String loginPage(@RequestParam(value = "id", defaultValue = "some user's id") String id) {
         return id;
     }
-    @RequestMapping(value="/logout",method= RequestMethod.GET)
+
+    @RequestMapping(value = "/logout", method = RequestMethod.GET)
     public String logoutPage() {
+
         return "Logout Page";
     }
 }
