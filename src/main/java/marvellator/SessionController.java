5ec2ecc02daf8dc84828b2e884c2108c28c19fa7
Bianca Petracca <biancapetracca@gmail.com>
2016-04-12 23:46:10 -0300

    Fixed controllers

--- src/main/java/marvellator/SessionController.java
+++ src/main/java/marvellator/SessionController.java
@@ -7,24 +7,24 @@ public class SessionController {
     /* API */
     @RequestMapping(value = "api/login", method = RequestMethod.POST)
     public String login() {
-        return "Login page";
+        return "Login page (API) \n";
     }
 
     @RequestMapping(value = "api/logout", method = RequestMethod.POST)
     public String logout() {
 
-        return "User signed out";
+        return "User signed out \n";
     }
 
     /* FRONT END */
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String loginPage(@RequestParam(value = "id", defaultValue = "some user's id") String id) {
-        return id;
+        return "Login page, the id is: " + id + "\n";
     }
 
     @RequestMapping(value = "/logout", method = RequestMethod.GET)
     public String logoutPage() {
 
-        return "Logout Page";
+        return "Logout Page \n";
     }
 }
