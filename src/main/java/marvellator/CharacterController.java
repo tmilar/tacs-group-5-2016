eae7c44898d625021ca78fbc96ed4e1c6c21eccc
Bianca Petracca <biancapetracca@gmail.com>
2016-04-12 23:46:10 -0300

    Fixed controllers
    Former-commit-id: a5246d0f09fbb3001d023ff4ebcb60c7e17cb901

--- src/main/java/marvellator/CharacterController.java
+++ src/main/java/marvellator/CharacterController.java
@@ -10,27 +10,27 @@ public class CharacterController {
     /* API */
     @RequestMapping(value = "api/characters", method = RequestMethod.GET)
     public String characters() {
-        return "List of characters";
+        return "List of characters \n";
     }
 
     @RequestMapping(value = "api/character/{id}", method = RequestMethod.POST)
     public String setAsFavorite(@PathVariable int id) {
-        return "Character set as favorite. Its id is: " + id;
+        return "Character set as favorite. Its id is: " + id + "\n";
     }
 
     @RequestMapping(value = "/api/ranking", method = RequestMethod.GET)
     public String ranking() {
-        return "Ranking of users";
+        return "Ranking of users \n";
     }
 
     /* FRONT END */
-    @RequestMapping(value = "characters", method = RequestMethod.GET)
+    @RequestMapping(value = "/characters", method = RequestMethod.GET)
     public String charactersPage() {
-        return "Characters Page";
+        return "Characters Page \n";
     }
 
     @RequestMapping(value = "/ranking", method = RequestMethod.GET)
     public String rankingPage() {
-        return "Ranking Page";
+        return "Ranking Page \n";
     }
 }
