57e8a3d5b01a83130608551ac40a8fefd56967e7
marcos287900 <marcos287900@gmail.com>
2016-04-15 01:02:52 -0300

    HomeController devuelve el template de home. Seteando basea contenido estatico en el web.xml

--- src/main/java/marvellator/HomeController.java
+++ src/main/java/marvellator/HomeController.java
@@ -2,12 +2,12 @@ package marvellator;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 
-@RestController
+@Controller
 public class HomeController {
     @RequestMapping(value = "/")
     public String home() {
-        return "Home page";
+        return "index";
     }
 }
