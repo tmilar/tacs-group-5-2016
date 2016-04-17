8bcef6cd87bd24054189206fed86d68b7d9f993c
Bianca Petracca <biancapetracca@gmail.com>
2016-04-17 03:06:58 -0300

    Adds signup + templates
    Former-commit-id: 2e52813373980378fc3ffe92079cf2a4acf21abe

--- src/main/java/marvellator/GroupController.java
+++ src/main/java/marvellator/GroupController.java
@@ -1,46 +1,23 @@
 package marvellator;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class GroupController {
-    /* API */
-    @RequestMapping(value = "api/groups", method = RequestMethod.GET)
-    public String groups() {
-        return "List of groups \n";
-    }
-
-    @RequestMapping(value = "api/groups", method = RequestMethod.POST)
-    public String newGroup() {
-        return "New group \n";
-    }
-
-    @RequestMapping(value = "api/groups/{id}", method = RequestMethod.GET)
-    public String group(@PathVariable int id) {
-        return "A group \n";
-    }
-
-    @RequestMapping(value = "api/groups/{id}", method = RequestMethod.PUT)
-    public String addCharacter(@PathVariable int id) {
-        return "Character added to a group \n";
-    }
-
-    @RequestMapping(value = "api/groups/{id}", method = RequestMethod.DELETE)
-    public String deleteGroup(@PathVariable int id) {
-        return "Group deleted \n";
-    }
-
-    /* FRONT END */
-    @RequestMapping(value = "/groups", method = RequestMethod.GET)
-    public String groupsPage() {
-        return "Groups Page \n";
-    }
 
     @RequestMapping(value = "/groups/{id}", method = RequestMethod.GET)
     public String groupPage(@PathVariable int id) {
         return "A group Page \n";
     }
+
+    @RequestMapping(value = "/groups", method = RequestMethod.GET)
+    public String groupsPage() {
+        return "Groups Page \n";
+    }
+
+
 }
