16add2a395cd1f394b29e1f759ca19e37171298f
Sebastian Bruno <sebastianbruno.exe@gmail.com>
2016-04-23 13:44:18 -0300

    Implementa Marcar/Desmarcar Favoritos
    Former-commit-id: 29233781347392f66c7df029832a716101b7bd62

--- src/test/java/org/utn/marvellator/service/UserServiceTest.java
+++ src/test/java/org/utn/marvellator/service/UserServiceTest.java
@@ -10,6 +10,8 @@ import org.utn.marvellator.model.User;
 import org.utn.marvellator.repository.UserRepository;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = ApplicationTest.class)
@@ -34,4 +36,28 @@ public class UserServiceTest {
         assertEquals(testUsername, userRepository.findFirstByUserName(testUsername).getUserName());
     }
 
+    @Test
+    public void addFav_withATestUser_shouldAssignAndPersistFavorite(){
+        String testUsername = "testUserName2";
+        int testCharacterId = 1;
+        User testUser = new User(testUsername);
+        testUser.addFavorite(testCharacterId);
+        userService.registerUser(testUser);
+
+        assertEquals(1, userRepository.findFirstByUserName(testUsername).getFavorites().size());
+        assertTrue(userRepository.findFirstByUserName(testUsername).getFavorites().contains(testCharacterId));
+    }
+
+    @Test
+    public void removeFav_withATestUser_shouldRemoveAndNoLongerPersistFavorite(){
+        String testUsername = "testUserName3";
+        int testCharacterId = 1;
+        User testUser = new User(testUsername);
+        testUser.addFavorite(testCharacterId);
+        testUser.removeFavorite(testCharacterId);
+        userService.registerUser(testUser);
+
+        assertEquals(0, userRepository.findFirstByUserName(testUsername).getFavorites().size());
+        assertFalse(userRepository.findFirstByUserName(testUsername).getFavorites().contains(testCharacterId));
+    }
 }
