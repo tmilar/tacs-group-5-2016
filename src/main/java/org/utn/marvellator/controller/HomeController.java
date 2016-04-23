4b32f266ccda42148afef284a27af1901ec77451
tmilar <tomasmilar4@gmail.com>
2016-04-23 17:22:34 -0300

    Add logic and rendering to index.html initial view
    Former-commit-id: b6c76a2f06b234689f0472f80a352b3c19a71c91

--- src/main/java/org/utn/marvellator/controller/HomeController.java
+++ src/main/java/org/utn/marvellator/controller/HomeController.java
@@ -1,8 +1,11 @@
 package org.utn.marvellator.controller;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.stereotype.Controller;
+
+import java.time.LocalDateTime;
 
 @Controller
 public class HomeController {
@@ -11,4 +14,11 @@ public class HomeController {
     public String home() {
         return "home";
     }
+
+    @RequestMapping(value = "/index")
+    public String index(Model model) {
+        model.addAttribute("now", LocalDateTime.now());
+        return "index";
+    }
+
 }
