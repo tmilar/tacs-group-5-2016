bd9aac22850885bdfac806170c38ec9fba099571
Sebastian Bruno <sebastianbruno.exe@gmail.com>
2016-04-30 16:24:50 -0300

    Agrega tests para grupos
    Former-commit-id: 6c12d106fc579f01759be9d33e10258865a79395

--- src/test/java/org/utn/marvellator/repository/GroupRepositoryTest.java
+++ src/test/java/org/utn/marvellator/repository/GroupRepositoryTest.java
@@ -21,6 +21,40 @@ public class GroupRepositoryTest {
     @Test
     public void shouldCreateAndPersistGroup() {
         String testName = "testGroup";
+        Group testGroup = new Group(testName);
+
+        groupRepository.save(testGroup);
+
+        assertEquals(1, groupRepository.count());
+        assertEquals(testName, groupRepository.findFirstByName(testName).getName());
+    }
+
+    @Test
+    public void shouldModifyAndPersistAGroupNameChange(){
+        Group testGroup = new Group("testName1");
+        groupRepository.save(testGroup);
+
+        String testName2 = "testName2";
+        testGroup.setName(testName2);
+        groupRepository.save(testGroup);
+
+        assertEquals(1, groupRepository.count());
+        assertEquals(testName2, groupRepository.findFirstByName(testName2).getName());
+    }
+
+    @Test
+    public void shouldDeleteAndNoLongerPersistGroup() {
+        Group testGroup = new Group();
+
+        groupRepository.save(testGroup);
+        groupRepository.delete(testGroup);
+
+        assertEquals(0, groupRepository.count());
+    }
+
+    @Test
+    public void shouldPersistCharacterInAGroup() {
+        String testName = "testGroup";
         int testCharacter = 666;
 
         Group testGroup = new Group(testName);
@@ -29,9 +63,23 @@ public class GroupRepositoryTest {
 
         groupRepository.save(testGroup);
 
-        assertEquals(1, groupRepository.count());
-        assertEquals(testName, groupRepository.findFirstByName(testName).getName());
         assertEquals(1, groupRepository.findFirstByName(testName).getCharacters().size());
         assertTrue(groupRepository.findFirstByName(testName).getCharacters().contains(testCharacter));
+    }
+
+    @Test
+    public void shouldDeleteAndNoLongerPersistCharacterInAGroup() {
+        String testName = "testGroup";
+        int testCharacter = 666;
+
+        Group testGroup = new Group(testName);
+        testGroup.addCharacter(testCharacter);
+        groupRepository.save(testGroup);
+
+        testGroup.removeCharacter(testCharacter);
+        groupRepository.save(testGroup);
+
+        assertEquals(1, groupRepository.count());
+        assertEquals(0, groupRepository.findFirstByName(testName).getCharacters().size());
     }
 }
