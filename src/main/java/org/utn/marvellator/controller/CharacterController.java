84424bc474ab5e050fbe2b9039571472b5800ca8
Bianca Petracca <biancapetracca@gmail.com>
2016-05-01 21:24:14 -0300

    Adds list of characters to model
    Former-commit-id: 3daee51c385fb4eb21bd9abe89f2e51d6e3c335c

--- src/main/java/org/utn/marvellator/controller/CharacterController.java
+++ src/main/java/org/utn/marvellator/controller/CharacterController.java
@@ -1,21 +1,28 @@
 package org.utn.marvellator.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.utn.marvellator.service.impl.CharactersService;
 
 @Controller
 public class CharacterController {
 
+    @Autowired
+    CharactersService charactersService;
+
     @RequestMapping(value = "/characters", method = RequestMethod.GET)
-    public String charactersPage() {
-        return "Characters Page \n";
+    public String charactersPage(Model model) {
+        model.addAttribute(charactersService.getCharacters());
+        return "characters";
     }
 
     @RequestMapping(value = "/ranking", method = RequestMethod.GET)
-    public String rankingPage() {
-        return "Ranking Page \n";
+    public String rankingPage(Model model) {
+        return "ranking";
     }
 }
