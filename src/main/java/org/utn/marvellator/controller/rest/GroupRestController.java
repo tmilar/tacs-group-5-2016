e5ae04e3a473fe129964ef2904e392770c206b86
tmilar <tomasmilar4@gmail.com>
2016-05-03 08:53:46 -0300

    GroupRestController full rework, See commit description for details
    * Refactored base path /api/groups to Controller class, saving
    the need of having to repeat it for all endpoints
    
    * Namespacing groups within the creator username, for now, ie.
    /{creatorName}/{groupName} - this is to reflect that a user can have
    many groups of different names, and names of groups can be the
    same of different users.
    
    * Using HATEOAS principles for most endpoints, however still some
    logic paths are missing (return correct HttpStatus codes for different
    scenarios, etc)
    
    * Fully working required features in GroupRestController for this
    iteration :)
    
    
    Former-commit-id: 9eb8821ba800fed5b763bf264a4d1767d2c62008

--- src/main/java/org/utn/marvellator/controller/rest/GroupRestController.java
+++ src/main/java/org/utn/marvellator/controller/rest/GroupRestController.java
@@ -1,59 +1,82 @@
 package org.utn.marvellator.controller.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.utn.marvellator.model.CharacterAlreadyInGroupException;
 import org.utn.marvellator.model.Group;
 import org.utn.marvellator.model.MarvelCharacter;
 import org.utn.marvellator.repository.GroupRepository;
+import org.utn.marvellator.service.GroupService;
 
 import java.util.List;
 
 @RestController
+@RequestMapping(value = "api/groups")
 public class GroupRestController {
 
     @Autowired
     private GroupRepository groupRepository;
 
-    @RequestMapping(value = "api/groups/{id}", method = RequestMethod.GET)
-    public String group(@PathVariable String id) {
-        Group group = groupRepository.findFirstById(id);
-        return "A group";
+    @Autowired
+    private GroupService groupService;
+
+    @RequestMapping(value = "/{userName}/{groupName}", method = RequestMethod.GET)
+    public Group groupByUserAndName(@PathVariable String userName, @PathVariable String groupName) {
+        Group group = groupRepository.findFirstByNameAndCreator(groupName, userName);
+
+        return group;
     }
 
-    @RequestMapping(value = "api/groups/{id}", method = RequestMethod.PUT)
-    public String addCharacter(@PathVariable String id,@RequestParam("characterId") int idCharacter) throws CharacterAlreadyInGroupException {
-        Group group = groupRepository.findFirstById(id);
+    @RequestMapping(value = "/{userName}", method = RequestMethod.GET)
+    public List<Group> groupsByUser(@PathVariable String userName) {
+        List<Group> groups = groupRepository.findByCreator(userName);
+
+        return groups;
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public List<Group> groups() {
+        List<Group> groups = groupRepository.findAll();
+        return groups;
+    }
+
+    @RequestMapping(value = "/{userName}/{groupName}", method = RequestMethod.POST)
+    public Group createGroup(@PathVariable String userName,
+                           @PathVariable String groupName) {
+        Group group = groupService.createGroup(groupName, userName);
+
+        return group;
+    }
+
+    @RequestMapping(value = "/{userName}/{groupName}", method = RequestMethod.PUT)
+    public HttpEntity<Group> addCharacter(@PathVariable String userName,
+                                                   @PathVariable String groupName,
+                                                   @RequestBody MarvelCharacter character) throws CharacterAlreadyInGroupException {
+        Group group = groupRepository.findFirstByNameAndCreator(groupName, userName);
 
         if(group != null){
-            group.addCharacter(new MarvelCharacter(String.valueOf(idCharacter)));
-            groupRepository.save(group);
-            return "Character added to a group \n";
-        }else return "Failure";
+            MarvelCharacter newCharacter = new MarvelCharacter(character.getName());
+            Group updated = groupService.addCharacterToGroup(newCharacter, group);
+            ResponseEntity<Group> response = new ResponseEntity<Group>( updated, HttpStatus.CREATED ) ;
+
+            return response;
+        }
+
+        return new ResponseEntity<Group>( HttpStatus.NOT_FOUND );
     }
 
-    @RequestMapping(value = "api/groups/{id}", method = RequestMethod.DELETE)
-    public String deleteGroup(@PathVariable String id) {
-        Group group = groupRepository.findFirstById(id);
+    @RequestMapping(value = "/{userName}/{groupName}", method = RequestMethod.DELETE)
+    public String deleteGroup(@PathVariable String userName,
+                              @PathVariable String groupName) {
+        Group group = groupRepository.findFirstByNameAndCreator(groupName, userName);
+
 
         if(group != null){
             groupRepository.delete(group);
-            return "Group deleted \n";
-        }else return "Failure";
+            return "" + group +" deleted.";
+        }else return "" + groupName + " not found for user " + userName;
     }
-
-    @RequestMapping(value = "api/groups", method = RequestMethod.GET)
-    public String groups() {
-        List<Group> groups = groupRepository.findAll();
-        return "List of groups \n";
-    }
-
-    @RequestMapping(value = "api/groups", method = RequestMethod.POST)
-    public String newGroup(@RequestParam("name") String name) {
-        Group group = new Group(name);
-        groupRepository.save(group);
-        return "New group \n";
-    }
-
-
 }
