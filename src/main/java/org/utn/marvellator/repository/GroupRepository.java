ae8b28b020d18ca6bd118f31446c98f9c7ecaeb6
tmilar <tomasmilar4@gmail.com>
2016-05-03 08:52:36 -0300

    Add findFirstByNameAndCreator() and findByCreator() methods to GroupRepository
    Former-commit-id: 6f9861e616b10016c4df3c16b26c3b13e3ad4c5f

--- src/main/java/org/utn/marvellator/repository/GroupRepository.java
+++ src/main/java/org/utn/marvellator/repository/GroupRepository.java
@@ -4,10 +4,16 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 import org.utn.marvellator.model.Group;
 
+import java.util.List;
+
 @Repository
 public interface GroupRepository extends MongoRepository<Group, String>{
 
     Group findFirstById(String id);
     Group findFirstByName(String name);
     void deleteAll();
+
+    Group findFirstByNameAndCreator(String groupName, String userName);
+
+    List<Group> findByCreator(String userName);
 }
