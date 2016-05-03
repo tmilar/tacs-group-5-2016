c36063df126f83bc1815474e3c493889d1f549d0
tmilar <tomasmilar4@gmail.com>
2016-05-03 01:30:53 -0300

    Add MarvelCharacter 'name' empty string + constructor initialization
    Former-commit-id: d26947f95f3db52c682f27af6c2de09a5a8dead2

--- src/main/java/org/utn/marvellator/model/MarvelCharacter.java
+++ src/main/java/org/utn/marvellator/model/MarvelCharacter.java
@@ -6,7 +6,8 @@ package org.utn.marvellator.model;
 public class MarvelCharacter {
 
     private String id;
-    private String name;
+    private String name = "";
+
     public String getId() {
         return id;
     }
@@ -14,7 +15,6 @@ public class MarvelCharacter {
     public void setId(String id) {
         this.id = id;
     }
-
 
     public String getName() {
         return name;
@@ -25,7 +25,10 @@ public class MarvelCharacter {
     }
 
     public MarvelCharacter(){
+    }
 
+    public MarvelCharacter(String name){
+        this.name = name;
     }
 
 }
