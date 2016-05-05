d79d6115b23a3f856479446f627bc75c2ead312b
tmilar <tomasmilar4@gmail.com>
2016-05-05 02:22:59 -0300

    Change CharacterService method getCharacters to getCharactersPage.
    change interface method getCharacters() --> getCharactersPage(offset,
    limit) to better match marvel characters API.
    
    this means that to get characters, application needs to define the
    starting point of the collection (offset) and the amount to get from
    there (limit). Marvel API just won't return *all* characters in one go.

--- src/main/java/org/utn/marvellator/service/CharacterService.java
+++ src/main/java/org/utn/marvellator/service/CharacterService.java
@@ -3,17 +3,13 @@ package org.utn.marvellator.service;
 import org.utn.marvellator.model.MarvelCharacter;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.MalformedURLException;
 import java.security.NoSuchAlgorithmException;
 import java.util.List;
 
-/**
- * Created by Tomas on 5/2/2016.
- */
 public interface CharacterService {
 
-    List<MarvelCharacter> getCharacters() throws IOException, NoSuchAlgorithmException;
-
     MarvelCharacter getCharacterById(Integer characterId);
+
+    List<MarvelCharacter> getCharactersPage(int offset, int limit) throws NoSuchAlgorithmException, IOException;
+
 }
