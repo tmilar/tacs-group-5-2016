622764a262ea455775491064f57d6d8309b30a5c
Bianca Petracca <biancapetracca@gmail.com>
2016-05-09 00:01:26 -0300

    Fixes how it finds characters

--- src/main/java/org/utn/marvellator/controller/CharacterController.java
+++ src/main/java/org/utn/marvellator/controller/CharacterController.java
@@ -5,7 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.utn.marvellator.model.UserSession;
 import org.utn.marvellator.service.CharacterService;
+import org.utn.marvellator.service.FavoritesService;
 
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
@@ -16,6 +18,12 @@ public class CharacterController {
     @Autowired
     CharacterService characterService;
 
+    @Autowired
+    FavoritesService favoritesService;
+
+    @Autowired
+    UserSession loggedUser;
+
     @RequestMapping(value = "/characters", method = RequestMethod.GET)
     public String characters(Model model) throws IOException, NoSuchAlgorithmException {
         int offset = 0;
@@ -24,6 +32,15 @@ public class CharacterController {
         model.addAttribute(characterService.getCharactersPage(offset, limit));
         return "characters";
     }
+
+    @RequestMapping(value = "/favorites", method = RequestMethod.GET)
+    public String favoritesPage(Model model) {
+        //  String username = loggedUser.getUser().getUserName();
+        model.addAttribute(favoritesService.getFavorites("eminem"));
+        return "favorites";
+        // return username;
+    }
+
 
     @RequestMapping(value = "/ranking", method = RequestMethod.GET)
     public String ranking(Model model) {
--- src/main/java/org/utn/marvellator/controller/rest/UserRestController.java
+++ src/main/java/org/utn/marvellator/controller/rest/UserRestController.java
@@ -2,21 +2,27 @@ package org.utn.marvellator.controller.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.utn.marvellator.model.CharacterAlreadyFavoritedException;
 import org.utn.marvellator.model.MarvelCharacter;
+import org.utn.marvellator.service.CharacterService;
 import org.utn.marvellator.service.FavoritesService;
 
 import java.util.List;
+import java.util.Set;
 
 @RestController
 public class UserRestController {
 
     @Autowired
+    private CharacterService characterService;
+    @Autowired
     private FavoritesService favoritesService;
 
 
     @RequestMapping(value = "api/users/{username}/favorites", method = RequestMethod.DELETE)
-    public MarvelCharacter deleteFavorite(@PathVariable String username, @RequestParam("characterId") Integer idCharacter) {
-        return favoritesService.removeFavorite(username, idCharacter);
+    public MarvelCharacter deleteFavorite(@PathVariable String username, @RequestParam("characterId") String idCharacter) {
+        MarvelCharacter character = characterService.getCharacterById(idCharacter);
+        return favoritesService.removeFavorite(username, character);
     }
 
     @RequestMapping(value = "api/users/{username}/favorites", method = RequestMethod.GET)
@@ -26,7 +32,8 @@ public class UserRestController {
     }
 
     @RequestMapping(value = "api/users/{username}/favorites", method = RequestMethod.POST)
-    public MarvelCharacter addFavorite(@PathVariable String username, @RequestParam("characterId") Integer idCharacter){
-       return favoritesService.addFavorite(username, idCharacter);
+    public MarvelCharacter addFavorite(@PathVariable String username, @RequestParam("characterId") String idCharacter) throws CharacterAlreadyFavoritedException{
+        MarvelCharacter character = characterService.getCharacterById(idCharacter);
+        return favoritesService.addFavorite(username, character);
     }
 }
