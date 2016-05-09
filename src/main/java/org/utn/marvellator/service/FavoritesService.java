890cd5079812e1db5f1b69ef6d215872751a887d
Bianca Petracca <biancapetracca@gmail.com>
2016-05-09 00:02:32 -0300

    Adds feature add favorite or remove it

--- src/main/java/org/utn/marvellator/service/FavoritesService.java
+++ src/main/java/org/utn/marvellator/service/FavoritesService.java
@@ -1,14 +1,15 @@
 package org.utn.marvellator.service;
 
+import org.utn.marvellator.model.CharacterAlreadyFavoritedException;
 import org.utn.marvellator.model.MarvelCharacter;
-
 import java.util.List;
+
 
 public interface FavoritesService {
     List<MarvelCharacter> getFavorites(String username);
 
-    MarvelCharacter addFavorite(String username, Integer character);
+    MarvelCharacter addFavorite(String username, MarvelCharacter character) throws CharacterAlreadyFavoritedException;
 
-    MarvelCharacter removeFavorite(String username, Integer character);
+    MarvelCharacter removeFavorite(String username, MarvelCharacter character);
 }
 
