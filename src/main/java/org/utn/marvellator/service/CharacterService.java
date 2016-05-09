9afc00bf7b319c7a3e067487d345e0d1e6bf5c5a
Bianca Petracca <biancapetracca@gmail.com>
2016-05-09 00:00:06 -0300

    Adds method find by id

--- src/main/java/org/utn/marvellator/service/CharacterService.java
+++ src/main/java/org/utn/marvellator/service/CharacterService.java
@@ -10,6 +10,8 @@ public interface CharacterService {
 
     MarvelCharacter getCharacterById(Integer characterId);
 
+    MarvelCharacter getCharacterById(String characterId);
+
     List<MarvelCharacter> getCharactersPage(int offset, int limit) throws NoSuchAlgorithmException, IOException;
 
     Integer getTotalCharacters() throws IOException, NoSuchAlgorithmException;
