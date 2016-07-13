4882720f0e139253fc2401b92a8401474ff69253
tmilar <milar@strands.com>
2016-07-13 14:43:40 -0300

    Make Application extend 'SpringBootServletInitializer' for tomcat deployments

--- src/main/java/org/utn/marvellator/Application.java
+++ src/main/java/org/utn/marvellator/Application.java
@@ -3,6 +3,8 @@ package org.utn.marvellator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
@@ -12,8 +14,12 @@ import org.utn.marvellator.config.*;
 @SpringBootApplication
 @EnableAutoConfiguration
 @Import(SpringSecurityConfig.class)
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
